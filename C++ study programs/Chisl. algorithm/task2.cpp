11ba532b6307d215f447439edf40c45a2b5ac965
BLeX <57190457+BLeX325@users.noreply.github.com>
2020-10-18 20:03:17 +0400

    Update task2.cpp

--- C++ study programs/Chisl. algorithm/task2.cpp
+++ C++ study programs/Chisl. algorithm/task2.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
-
 double Interpol (double* x, double* F_x, int N, double t){
-double Sum = 0;
-for (int i = 0; i < N; i++){
-double Proiz= 1;
-for (int j = 0; j < N; j++){
-if (!(j == i)){
-Proiz *= (t - x[j])/(x[i]-x[j]);
-}
-}
-Sum += (Proiz* F_x[i]);
+    double Sum = 0;
+    for (int i = 0; i < N; i++){
+        double Proiz= 1;
+        for (int j = 0; j < N; j++){
+            if (!(j == i)){
+            Proiz *= (t - x[j])/(x[i]-x[j]);
+            }
+        }
+    Sum += (Proiz* F_x[i]);
 }
 return Sum;
 }
@@ -22,10 +21,10 @@ cin >> n;
 double *x = new double [n];
 double *y = new double [n];
 for (int i = 0; i < n; i++) {
-cout << "Vvedite x["<< i+1 <<"]:"<< endl;
-cin >> x [i];
-cout << "Vvedite f(x)["<< i+1 <<"]:"<< endl;
-cin >> y [i];
+    cout << "Vvedite x["<< i+1 <<"]:"<< endl;
+    cin >> x [i];
+    cout << "Vvedite x["<< i+1 <<"]:"<< endl;
+    cin >> y [i];
 }
 double x0;
 cout << "Vvedite x - neobhodimyu vicheslit:"<< endl;
