eca05f801e34df5d21ca09622b72bb667a4851a9
BLeX <57190457+BLeX325@users.noreply.github.com>
2020-10-19 17:20:01 +0400

    Update task2.cpp

--- C++ study programs/Chisl. algorithm/task2.cpp
+++ C++ study programs/Chisl. algorithm/task2.cpp
@@ -27,14 +27,14 @@ double *x = new double [n]; // переменная для x
 double *y = new double [n]; // переменная для значения функции от x
 for (int i = 0; i < n; i++)  // цикл ввода значений x
 {
-    cout << endl <<  "x["<< i+1 <<"]:";
-    cin >> x [i];
+    cout << endl <<  "x["<< i+1 <<"]   = ";
+    cin >> x[i];
     y [i] = cosh(x[i]);
-    cout << "f(x)["<< i+1 <<"]:"<< y[i] << endl;
+    cout << "f(x)["<< i+1 <<"]= "<< y[i] << endl;
 }
 double x0;
-cout << endl << "Vvedite x - neobhodimyu vicheslit:"<< endl;
+cout << endl << "Vvedite x - neobhodimyu vicheslit:";
 cin >> x0;
-cout << "Rezultat:" << endl << Interpol (x,y,n,x0);
+cout << endl << "Rezultat:" << endl << Interpol (x,y,n,x0);
 return 0;
 }
