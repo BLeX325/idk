60f6f2d1b19782a5597dcaba726664d14ec32e71
BLeX <57190457+BLeX325@users.noreply.github.com>
2020-10-19 17:16:05 +0400

    Update task2.cpp

--- C++ study programs/Chisl. algorithm/task2.cpp
+++ C++ study programs/Chisl. algorithm/task2.cpp
@@ -2,17 +2,20 @@
 #include <cmath>
 using namespace std;
 
-double Interpol (double* x, double* F_x, int N, double t){
-    double Sum = 0;
-    for (int i = 0; i < N; i++){
-        double Proiz= 1;
-        for (int j = 0; j < N; j++){
-            if (!(j == i)){
-            Proiz *= (t - x[j])/(x[i]-x[j]);
+double Interpol (double* x, double* F_x, int N, double t) // функция подсчёта интерполяционного многочлена Лагранжа
+{
+    double Sum = 0; // переменная для подсчёта суммы произведений значений функции на базисный полином
+    for (int i = 0; i < N; i++) // цикл подсчёта инт. многочлена 
+    {
+        double P = 1; // Базисный полином
+        for (int j = 0; j < N; j++) // подсчёт базисного полинома
+        {
+            if (!(j == i)){ // если j не равно i то продолжаем умножение
+            P *= (t - x[j])/(x[i]-x[j]); // Базисный полином
             }
         }
-    Sum += (Proiz* F_x[i]);
-}
+    Sum += (P* F_x[i]);
+    }
 return Sum;
 }
 
@@ -20,19 +23,18 @@ int main(){
 int n;
 cout << "Vvedite chislo tochek:";
 cin >> n;
-double *x = new double [n];
-double *y = new double [n];
-for (int i = 0; i < n; i++) {
-    cout << "Vvedite x["<< i+1 <<"]:"<< endl;
+double *x = new double [n]; // переменная для x
+double *y = new double [n]; // переменная для значения функции от x
+for (int i = 0; i < n; i++)  // цикл ввода значений x
+{
+    cout << endl <<  "x["<< i+1 <<"]:";
     cin >> x [i];
-    // cout << "Vvedite x["<< i+1 <<"]:"<< endl;
-    //cin
     y [i] = cosh(x[i]);
     cout << "f(x)["<< i+1 <<"]:"<< y[i] << endl;
 }
 double x0;
-cout << "Vvedite x - neobhodimyu vicheslit:"<< endl;
+cout << endl << "Vvedite x - neobhodimyu vicheslit:"<< endl;
 cin >> x0;
-cout << "Itog:" << endl << Interpol (x,y,n,x0);
+cout << "Rezultat:" << endl << Interpol (x,y,n,x0);
 return 0;
 }
