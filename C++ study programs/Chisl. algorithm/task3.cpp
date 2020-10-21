dc1102d20f23f06022b6fadf686f93fa987826b2
BLeX <57190457+BLeX325@users.noreply.github.com>
2020-10-21 15:33:15 +0400

    Create task3.cpp

---
+++ C++ study programs/Chisl. algorithm/task3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double Func (double x)
+{
+    return exp(-1*x)*cos(x);
+}
+
+double Tr (double x, double h)
+{
+    return (fabs(Func(x)+Func(x+h)))*(h)/2;
+}
+
+double Int_Tr (double a, double b, int n)
+{
+    double h = (b-a)/(n) ;
+    double Sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        Sum += Tr (a+i*h, h)
+    }
+    return Sum;
+}
+
+int Poisk (double eps = 1e-2)
+{
+    int n = 1;
+    for (; (1.0/(3*n*n*n)) > (eps / 2); n++){};
+    return n;
+}
+
+double Runge (double a = 0, double n = 4, double eps = 1e-2)
+{
+    double b = Poisk ();
+    double rez = Int_Tr(a,b,n);
+    double Sled = Int_Tr(a,b,2*n);
+    while ((fabs(rez - Sled))/3>= eps / 2)
+    {
+        rez = Sled;
+        n = n*2;
+        Sled = Int_Tr(a,b,2*n);
+    }
+    return rez;
+}
+
+int main(){
+int n;
+cout <<Poisk()<< endl<< Runge();
+cin >> n;
+return 0;
+}
